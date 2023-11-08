7214b37fe6c3978425de3cc29b053768e0c6f805
MansuttiSereno <daniel.mansutti@serenoapp.com>
2023-11-08 12:28:34 -0400

    feat: fb pixel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,20 @@
+import Head from 'next/head';
 import BoxFaqs from '../components/home/BoxFaqs';
 import Jointelegram from '../components/home/JoinTelegram';
 import PrincipalSection from '../components/home/PrincipalSection';
 import FooterPrincipal from '../components/shared/FooterPrincipal';
 import HeaderPrincipal from '../components/shared/HeaderPrincipal';
 
-
-
 export default function Home() {
   return (
     <>
+      <Head>
+        <script async defer src="/fb-pixel.js" />
+      </Head>
       <div className="bg-general_backgound">
         <div>
           <HeaderPrincipal/>
           <PrincipalSection/>
-          {/* <JoinSereno/> */}
-          {/* <PaymentMethods/> */}
-          {/* <Downloadsereno/> */}
-          {/* <TwitterSlider/> */}
           <BoxFaqs/>
           <Jointelegram/>
           <FooterPrincipal/>
@@ -25,3 +23,4 @@ export default function Home() {
     </>
   );
 }
+
