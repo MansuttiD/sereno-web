594c1bd212208c11781dcb33fcdf21b2ef5a0c39
Leandro Cardenas <leandro.cardenas@serenoapp.com>
2023-11-14 12:41:07 -0400

    Add Facebook Pixel script to layout and remove from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import BoxFaqs from '../components/home/BoxFaqs';
 import Jointelegram from '../components/home/JoinTelegram';
 import PrincipalSection from '../components/home/PrincipalSection';
@@ -8,9 +7,6 @@ import HeaderPrincipal from '../components/shared/HeaderPrincipal';
 export default function Home() {
   return (
     <>
-      <Head>
-        <script async defer src="/fb-pixel.js" />
-      </Head>
       <div className="bg-general_backgound">
         <div>
           <HeaderPrincipal/>
